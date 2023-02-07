d5bbf93654f8573f8f1f285c9d67877ad65ad5e0
freak906 <119906147+nick-vns@users.noreply.github.com>
2023-02-07 13:37:05 -0700

    added token imported login and signup

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,14 +1,25 @@
 import { useEffect, useState } from "react";
+import { LoginView } from "../login-view/login-view";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
   const [movies, setMovies] = useState ([]);
-
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
-    fetch("https://myflix-app.herokuapp.com/movies")
+    if (!token) {
+      return;
+    }
+
+    fetch("https://myflix-app.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}`}
+    })
     .then((response) => response.json())
     .then((data) => {
       const moviesFromApi = data.map((doc) => {
@@ -24,7 +35,21 @@ export const MainView = () => {
       });
       setMovies(moviesFromApi);
     });
-  }, []);
+  }, [token]);
+
+  if (!user) {
+    return ( 
+      <>
+    <LoginView
+     onLoggedIn={(user, token) => {
+      setUser(user);
+      setToken(token);
+    }} />
+    or
+    <SignupView />
+    </>
+    );
+  }
   
   if (selectedMovie) {
 
@@ -65,6 +90,7 @@ export const MainView = () => {
   }
   return (
     <div>
+      <button onClick={() => {setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
       {movies.map((movie) => (
         <MovieCard 
           key={movie.id}
