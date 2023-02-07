af94a8753bb4e177239ef89108a80c2e6015ef9a
freak906 <119906147+nick-vns@users.noreply.github.com>
2023-02-06 18:06:48 -0700

    added prop-types

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,26 +1,48 @@
+import PropTypes from "prop-types";
+
 export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
       <div>
-        <img src={movie.image} />
+        <img src={movie.ImagePath} />
       </div>
       <div>
         <span>Title: </span>
-        <span>{movie.title}</span>
+        <span>{movie.Title}</span>
       </div>
       <div>
         <span>Description: </span>
-        <span>{movie.description}</span>
+        <span>{movie.Description}</span>
       </div>
       <div>
         <span>Genre: </span>
-        <span>{movie.genre}</span>
+        <span>{movie.Genre.Name}</span>
       </div>
       <div>
       <span>Director: </span>
-      <span>{movie.director}</span>
+      <span>{movie.Director.Name}</span>
+    </div>
+    <div>
+      <span>Release: </span>
+      <span>{movie.Release}</span>
     </div>
     <button onClick={onBackClick}>Back</button>
     </div>
   );
+};
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string,
+    Description: PropTypes.string,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string,
+      Description: PropTypes.string
+    }).isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string,
+      Bio: PropTypes.string
+    }).isRequired,
+    Release: PropTypes.string
+  }).isRequired,
 };
