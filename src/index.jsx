8a2b2cd6cebc3a1fb08143e62fd0490c7c4e7b2a
freak906 <119906147+nick-vns@users.noreply.github.com>
2023-02-22 11:14:29 -0700

    extra comments

--- src/index.jsx
+++ src/index.jsx
@@ -11,8 +11,7 @@ const MyFlixApplication = () => {
   );
 };
 
-//finds root of my app
 const container = document.querySelector("#root");
 const root = createRoot(container);
-//tells react to render my app in root DOM element
+
 root.render(<MyFlixApplication />);
